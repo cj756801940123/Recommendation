f19bb5a586a5332a19d5a3f715e3345722a3611d
Cccj <cj756801940@163.com>
2018-05-10 18:04:39 +0800

    商品设计详情界面中的评论部分

--- Recommedation/update/update_info.py
+++ Recommedation/update/update_info.py
@@ -32,8 +32,25 @@ def update_score(user,table):
     data =[user,table]
     result = database_util
 
+def update_img(table):
+    sql = 'select img,sku from '+table
+    result = database_util.search_sql(sql, None)
+    if result[0]!=-1:
+        imgs = list(result[1])
+        for i in imgs:
+            img = i[0]
+            sku = i[1]
+            print(img)
+            new_img = img.replace('n5/s54x54_jfs','n7/jfs')
+            print(new_img+'\n')
+
+            sql = 'update '+table+' set img=%s where sku=%s'
+            data = [new_img,sku]
+            database_util.update_sql(sql,data)
+
+# https://img11.360buyimg.com/n5/s54x54_jfs/t5773/143/1465870132/216483/4bbce005/592692d8Nbcc8f248.jpg
+# https://img10.360buyimg.com/n7/jfs/t18772/89/1863054684/170815/d28ecae1/5adca3deN76bb61cb.jpg
 
 if __name__ == '__main__':
     table = 'cellphone'
-    del_file(table)
-    # del_items(table)
+    update_img(table)
